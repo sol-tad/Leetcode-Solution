81eb053866149db12e1d98cc76e8218eaac30117
Solomon Tadesse <122595521+sol-tad@users.noreply.github.com>
2024-10-09 07:24:24 -0700

    Time: 30 ms (95.18%), Space: 16.5 MB (58.16%) - LeetHub

--- 0043-multiply-strings/0043-multiply-strings.py
+++ 0043-multiply-strings/0043-multiply-strings.py
@@ -1,5 +1,5 @@
 class Solution:
     def multiply(self, num1: str, num2: str) -> str:
-        sum=int(num1)*int(num2)
-        return str(sum)
+        mul=int(num1)*int(num2)
+        return str(mul)
         
