81fbb927bc0b445289ddf8a69e2f13e2f84c66e5
Solomon Tadesse <122595521+sol-tad@users.noreply.github.com>
2024-10-11 07:41:12 -0700

    Time: 267 ms (46.74%), Space: 25 MB (41.38%) - LeetHub

--- 0442-find-all-duplicates-in-an-array/0442-find-all-duplicates-in-an-array.py
+++ 0442-find-all-duplicates-in-an-array/0442-find-all-duplicates-in-an-array.py
@@ -1,8 +1,12 @@
 class Solution:
-    def findDuplicates(self, nums: List[int]) -> List[int]:
-        nset=set(nums)
-        res=[]
-        for num in nset:
-            if nums.count(num)==2:
-                res.append(num)
+    def findDuplicates(self, nums: list[int]) -> list[int]:
+        res = []
+        
+        for num in nums:
+            index = abs(num) - 1  # Get the index corresponding to the value
+            if nums[index] < 0:
+                res.append(abs(num))  # If already negative, it's a duplicate
+            else:
+                nums[index] = -nums[index]  # Mark it as visited by negating it
+                
         return res
