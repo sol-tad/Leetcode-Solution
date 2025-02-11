f719ab1716f905b9069ba84b06ed61851c40d645
Solomon Tadesse <122595521+sol-tad@users.noreply.github.com>
2025-02-11 14:14:55 +0300

    Time: 82 ms (88.42%), Space: 24.2 MB (80.18%) - LeetHub

--- 0912-sort-an-array/0912-sort-an-array.py
+++ 0912-sort-an-array/0912-sort-an-array.py
@@ -1,3 +1,15 @@
 class Solution:
     def sortArray(self, nums: List[int]) -> List[int]:
-        return sorted(nums)
+            
+        maximum = max(nums)
+        minimum = abs(min(nums))
+        count = [0] * (maximum + minimum + 1)
+        for num in nums:
+            count[num + minimum] += 1
+        target = 0
+
+        for index, value in enumerate(count):
+            for i in range(value):
+                nums[target] = index - minimum
+                target += 1
+        return nums
