6a3f50319467ef0342c2dfc6a8e3b566de96f8cc
Solomon Tadesse <122595521+sol-tad@users.noreply.github.com>
2025-02-13 15:20:26 +0300

    Time: 119 ms (23.62%), Space: 18 MB (38.14%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.py
+++ 0567-permutation-in-string/0567-permutation-in-string.py
@@ -3,22 +3,25 @@ class Solution:
         n=len(s1)
         hm=dict(Counter(s1))
         window=dict(Counter(s2[:n]))
-        l,r=0,n-1
+        l,r=0,n
       
-
-        while r<len(s2)-1:
-            r+=1
+        while r<len(s2):
+            
             if hm==window:
                 return True
             if s2[r] in window:
                 window[s2[r]]+=1
             else:
                 window[s2[r]]=1
-            del window[s2[l]]
+            
+            window[s2[l]]-=1
+            if window[s2[l]]==0:
+                del window[s2[l]]           
+
             l+=1
-           
+            r+=1
             print(window)
-        return False
+        return hm==window
 
 
 
