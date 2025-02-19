5905bda9b0de29d8ad63e8ab4ac14cc1c9c0bd0b
Solomon Tadesse <122595521+sol-tad@users.noreply.github.com>
2025-02-19 14:35:11 +0300

    Time: 34 ms (40.61%), Space: 21.5 MB (32.99%) - LeetHub

--- 0930-binary-subarrays-with-sum/0930-binary-subarrays-with-sum.py
+++ 0930-binary-subarrays-with-sum/0930-binary-subarrays-with-sum.py
@@ -1,26 +1,14 @@
 class Solution:
-    
-    def slwAtmost(self,nums,k):
-        l=0
-        currsum=0
-        count=0
-        for r in range(len(nums)):
-            currsum+=nums[r]
-            while l<=r and currsum>k:
-                currsum-=nums[l]
-                l+=1
-            count+=r-l+1
-        return count
     def numSubarraysWithSum(self, nums: List[int], goal: int) -> int:
-        counGoal=self.slwAtmost(nums,goal)
-        counGoal_1=self.slwAtmost(nums,goal-1)
-        return counGoal-counGoal_1
-    
-    
-    
-    
+        count=0
+        currsum=0
+        hm=defaultdict(int)
 
-
-
-
-        
+        for i in range(len(nums)):
+            currsum+=nums[i]
+            if currsum==goal:
+                count+=1
+            if currsum-goal in hm:
+                count+=hm[currsum-goal]
+            hm[currsum]+=1
+        return count
