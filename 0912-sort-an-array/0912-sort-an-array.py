8a876aaf44bc9eed1e397ac8295b14d3f3bdfe84
Solomon Tadesse <122595521+sol-tad@users.noreply.github.com>
2025-03-31 10:44:25 +0300

    Time: 597 ms (63.72%), Space: 25.5 MB (40.12%) - LeetHub

--- 0912-sort-an-array/0912-sort-an-array.py
+++ 0912-sort-an-array/0912-sort-an-array.py
@@ -1,15 +1,26 @@
 class Solution:
     def sortArray(self, nums: List[int]) -> List[int]:
             
-        maximum = max(nums)
-        minimum = abs(min(nums))
-        count = [0] * (maximum + minimum + 1)
-        for num in nums:
-            count[num + minimum] += 1
-        target = 0
-
-        for index, value in enumerate(count):
-            for i in range(value):
-                nums[target] = index - minimum
-                target += 1
-        return nums
+        def merge(lH,rH):
+            r=l=0
+            mergedarr=[]
+            while l<len(lH) and r<len(rH):
+                if lH[l]<rH[r]:
+                    mergedarr.append(lH[l])
+                    l+=1
+                else:
+                    mergedarr.append(rH[r])
+                    r+=1
+            mergedarr.extend(lH[l:])
+            mergedarr.extend(rH[r:])
+            return mergedarr
+        
+        def mergeSort(left,right,arr):
+            if left==right:
+                return[arr[left]]
+            mid=left+(right-left)//2
+            leftHalf=mergeSort(left,mid,arr)
+            rightHalf=mergeSort(mid+1,right,arr)
+            return merge(leftHalf,rightHalf)
+        return mergeSort(0,len(nums)-1,nums)
+                
