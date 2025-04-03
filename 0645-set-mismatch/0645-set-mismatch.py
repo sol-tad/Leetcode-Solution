8d2ea31c487e2cc940db6433e9a1db969b29da12
Solomon Tadesse <122595521+sol-tad@users.noreply.github.com>
2025-04-03 20:13:35 +0300

    Time: 33 ms (19.21%), Space: 19.1 MB (50.63%) - LeetHub

--- 0645-set-mismatch/0645-set-mismatch.py
+++ 0645-set-mismatch/0645-set-mismatch.py
@@ -2,15 +2,25 @@ class Solution:
     def findErrorNums(self, nums: List[int]) -> List[int]:
         nums.sort()
         hm=Counter(nums)
+        print(nums)
         rep=-1
         mis=-1
-        i=1
+       
         for k in hm:
             if hm[k]>1:
                 rep= k
                 break
-        for i,num in enumerate(nums):
-            if i+1!=num:
-                mis=i+1
+        curr=1
+        i=0
+        while i< len(nums):
+            if nums[i]==rep:
+                i+=1
+            if curr!=nums[i]:
+                mis=curr
                 break
+            i+=1
+            curr+=1
+        if mis==-1:
+            mis=len(nums)
+            
         return [rep,mis]
