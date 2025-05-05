72b520762b953191ff50d1b27d167bac206cb2b4
Solomon Tadesse <122595521+sol-tad@users.noreply.github.com>
2025-05-05 22:16:31 +0300

    Time: 14 ms (22.66%), Space: 18.5 MB (78.06%) - LeetHub

--- 0724-find-pivot-index/0724-find-pivot-index.py
+++ 0724-find-pivot-index/0724-find-pivot-index.py
@@ -4,6 +4,7 @@ class Solution:
             nums[i]=nums[i]+nums[i-1]
         
         for i in range(len(nums)):
+
             left_sum=right_sum=0
             if i==0:
                 left_sum=0
@@ -11,6 +12,6 @@ class Solution:
                 left_sum=nums[i-1]
             right_sum=nums[len(nums)-1]-nums[i]
             if left_sum==right_sum:
-                return i
+                 return i
         return -1
         
