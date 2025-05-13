f5ec9367ce694d7fd2f70846f4c1c3a77c7943f1
Solomon Tadesse <122595521+sol-tad@users.noreply.github.com>
2025-05-13 19:41:24 +0300

    Time: 7 ms (38.16%), Space: 18.7 MB (51.92%) - LeetHub

--- 0268-missing-number/0268-missing-number.py
+++ 0268-missing-number/0268-missing-number.py
@@ -1,14 +1,21 @@
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
         # return ((len(nums)*(len(nums)+1))//2)-sum(nums)
-        nums.sort()
-        l=0
-        r=len(nums)
+        # nums.sort()
+        # l=0
+        # r=len(nums)
         
-        while l<r:
-            mid=(l+r)//2
-            if nums[mid]>mid:
-                r=nums[mid]-1
-            else:
-                l=mid+1
-        return l
+        # while l<r:
+        #     mid=(l+r)//2
+        #     if nums[mid]>mid:
+        #         r=nums[mid]-1
+        #     else:
+        #         l=mid+1
+        # return l
+
+        total=0
+        xorsum=0
+        for i in range(len(nums)):
+            total^=i+1
+            xorsum^=nums[i]
+        return xorsum^total
