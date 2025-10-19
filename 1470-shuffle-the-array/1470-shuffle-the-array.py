1aae3e1231ea2999ce90127b016e076569dd35e4
Solomon Tadesse <122595521+sol-tad@users.noreply.github.com>
2025-10-19 21:20:47 +0300

    Time: 61 ms (36.22%), Space: 18 MB (59.54%) - LeetHub

--- 1470-shuffle-the-array/1470-shuffle-the-array.py
+++ 1470-shuffle-the-array/1470-shuffle-the-array.py
@@ -3,7 +3,9 @@ class Solution:
         l1=nums[:n]
         l2=nums[n:]
         res=[]
+
         for i in range(n):
             res.append(l1[i])
+            
             res.append(l2[i])
         return res
