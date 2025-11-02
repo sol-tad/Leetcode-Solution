12921e4cdd73b3f2179e48b3fec44c2232b5b2eb
Solomon Tadesse <122595521+sol-tad@users.noreply.github.com>
2025-11-02 11:46:16 +0300

    Time: 51 ms (25.82%), Space: 19.1 MB (90.81%) - LeetHub

---
+++ 0559-maximum-depth-of-n-ary-tree/0559-maximum-depth-of-n-ary-tree.py
@@ -0,0 +1,22 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val: Optional[int] = None, children: Optional[List['Node']] = None):
+        self.val = val
+        self.children = children
+"""
+
+class Solution:
+    def maxDepth(self, root: 'Node') -> int:
+        if not root:
+            return 0
+        depth = 0
+        queue = deque([root])
+
+        while queue:
+            depth += 1
+            for _ in range(len(queue)):
+                node = queue.popleft()
+                for child in node.children:
+                    queue.append(child)
+        return depth
